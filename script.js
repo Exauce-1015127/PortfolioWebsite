28949f40a35633369e1deb241124f849cbd6a1cf
Exauce-1015127 <1015127@hr.nl>
2026-01-15 01:36:30 +0100

    Created a setup for the homepage of my portfolio website with the help of 'Codehal' YouTube tutorial.
    This includes:
    - Index.html (homepage)
    - Script.js (This Javascript file contains code for the typing animation)
    - Style.css (Stylesheet)

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Web Developer", "UI/UX Designer", "YouTuber"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
